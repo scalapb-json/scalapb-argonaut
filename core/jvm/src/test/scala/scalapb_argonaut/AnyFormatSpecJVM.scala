6c2377066418258047f5a82d06952b0ea2544378
xuwei-k <6b656e6a69@gmail.com>
2023-12-25 09:56:46 +0900

    add `-Xsource:3`

--- core/jvm/src/test/scala/scalapb_argonaut/AnyFormatSpecJVM.scala
+++ core/jvm/src/test/scala/scalapb_argonaut/AnyFormatSpecJVM.scala
@@ -3,9 +3,10 @@ package scalapb_argonaut
 import com.google.protobuf.any.{Any => PBAny}
 import utest._
 import jsontest.anytests.AnyTest
+import scalapb.GeneratedMessageCompanion
 
 object AnyFormatSpecJVM extends TestSuite with JavaAssertions {
-  override def registeredCompanions = Seq(AnyTest)
+  override def registeredCompanions: Seq[GeneratedMessageCompanion[?]] = Seq(AnyTest)
 
   override val tests = Tests {
     "Any should be serialized the same as in Java (and parsed back to original)" - {
